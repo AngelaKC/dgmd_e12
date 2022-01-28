5361dbadf9f75aa247bc7996ab0a99d0be907521
Angela Carey <angela.k.carey@gmail.com>
2022-01-28 14:50:04 -0500

    added week 1 informal complete

--- week01_informal/sketch.js
+++ week01_informal/sketch.js
@@ -1,16 +1,69 @@
-let xPos = 200;
-let yPos = 300;
-
 function setup() {
   createCanvas(600, 600);
 }
 
 function draw() {
-  background(128);
-  ellipse(xPos, yPos, 150, 150);
-  // triangle(x1, y1, x2, y2, x3, y3)
-  rectMode(CENTER);
-  rect(100, 100, 50, 50);
-  rect(400, 100, 50, 50);
-  // xPos = xPos+1;
+  background(220);
+  // text("X: " + mouseX, 20, 30);
+  // text("Y: " + mouseY, 20, 50);
+
+  textStyle(normal);
+  textSize(24);
+  fill(250, 235, 205);
+  ellipse(300, 300, 150, 200);
+
+  // glasses top
+  line(245, 280, 355, 280);
+  // left eye
+  ellipse();
+  ellipseMode(RADIUS);
+  fill(250, 235, 205);
+  ellipse(275, 300, 20, 18); // Outer glasses
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(275, 300, 22, 8); // outer eyeball
+  ellipseMode(CENTER);
+  fill(47, 79, 79);
+  ellipse(275, 300, 3, 3); // Inner eyeball
+  // right eye
+  ellipseMode(RADIUS);
+  fill(250, 235, 205);
+  ellipse(325, 300, 20, 18); // Outer glasses
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(325, 300, 22, 8); // outer eyeball
+  ellipseMode(CENTER);
+  fill(47, 79, 79);
+  ellipse(325, 300, 3, 3); // Inner eyeball
+  // nose
+  fill(250, 235, 205);
+  triangle(300, 290, 295, 330, 305, 330);
+  //mouth
+  noFill();
+  beginShape();
+  vertex(280, 350);
+  vertex(325, 350);
+  vertex(330, 348);
+  endShape();
+  // hair left
+  fill(169, 169, 169);
+  beginShape(TRIANGLES);
+  vertex(210, 375);
+  vertex(230, 255);
+  vertex(240, 375);
+  endShape();
+  // hair right
+  beginShape(TRIANGLES);
+  vertex(360, 375);
+  vertex(370, 255);
+  vertex(390, 375);
+  endShape();
+  // hat
+  fill(59, 43, 68);
+  beginShape(TRIANGLES);
+  vertex(215, 275);
+  vertex(390, 275);
+  vertex(300, 155);
+  endShape();
+  ellipse(300, 155, 30, 30);
 }
