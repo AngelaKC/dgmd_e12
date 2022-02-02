583f25e5d1e310d661f119e4bf4b48459d86ab31
Angela Carey <angela.k.carey@gmail.com>
2022-02-02 15:30:16 -0500

    created function

--- informal/week02/sketch.js
+++ informal/week02/sketch.js
@@ -1,64 +1,49 @@
 // the initial rotation speed for the windmills
-let millSpeed1 = 0;
-let millSpeed2 = 0;
+let millSpeedy = 0;
+let millSpeedx = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
+//59,43,68 -- purple
 
 function draw() {
   background(220);
+  windmill(6,millSpeedx,"x","rgb(59,43,68)","rgb(97, 139, 74)");
+  windmill(2,millSpeedy,"y","rgb(154, 140, 152)","rgb(124, 165, 184)");
+  // adjust speed of windmills bases on mouse moving accross x and y axis
+  millSpeedx += 5 + map(mouseX, 0, windowWidth, -10, 10);
+  millSpeedy += 5 + map(mouseY, 0, windowHeight, -10, 10);
+}
 
+function windmill(loc,sp,ax,baseColor,bladeColor){
   // windmill 1
   push();
   // move the origin to the left of the canvas
-  translate(windowWidth / 6, windowHeight / 6);
+  translate(windowWidth / loc, windowHeight / loc);
   // windmill base
-  fill(59, 43, 68); // purple
+  fill(baseColor);
   beginShape();
   vertex(-15, -15);
   vertex(-50, 200);
   vertex(50, 200);
   vertex(15, -15);
   endShape(CLOSE);
-  text("Speed controlled by x-axis", -75, 220);
+  fill(0);  // black text
+  text("Speed controlled by "+ax+"-axis", -75, 220);
 
   // windmill blades
-  fill(255);   // white
-  rotate(radians(millSpeed1));
+  fill(bladeColor);   
+  rotate(radians(sp));
   triangle(0, 0, 100, 50, 50, 100);
   triangle(0, 0, -100, -50, -50, -100);
   triangle(0, 0, -100, 50, -50, 100);
   triangle(0, 0, 100, -50, 50, -100);
 
   ellipse(0, 0, 30,);
+  // sp += 5 + map(mouseX, 0, windowWidth, -10, 10);
 
   pop();
 
-  // windmill 2
-  push();
-  // move the origin to the center of the canvas
-  translate(windowWidth / 2, windowHeight / 2);
-  // windmill base
-  beginShape();
-  vertex(-15, -15);
-  vertex(-50, 200);
-  vertex(50, 200);
-  vertex(15, -15);
-  endShape(CLOSE);
-  text("Speed controlled by y-axis", -75, 220);
-  // windmill blades
-  fill(59, 43, 68);  // purple
-  rotate(radians(millSpeed2));
-  triangle(0, 0, 100, 50, 50, 100);
-  triangle(0, 0, -100, -50, -50, -100);
-  triangle(0, 0, -100, 50, -50, 100);
-  triangle(0, 0, 100, -50, 50, -100);
-  fill(59, 43, 68); // purple
-  ellipse(0, 0, 30,);
-  pop();
-
-  // adjust speed of windmills bases on mouse moving accross x and y axis
-  millSpeed1 += 5 + map(mouseX, 0, windowWidth, -10, 10);
-  millSpeed2 += 5 + map(mouseY, 0, windowHeight, -10, 10);
 }
+
