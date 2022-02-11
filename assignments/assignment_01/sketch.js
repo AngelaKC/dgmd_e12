9dccf138034512bb7553708596b9d3fb5c98ea80
Angela Carey <angela.k.carey@gmail.com>
2022-02-11 11:25:55 -0500

    baseline shapes

--- assignments/assignment_01/sketch.js
+++ assignments/assignment_01/sketch.js
@@ -1,45 +1,24 @@
-// the initial rotation speed for the windmills
-let millSpeedy = 0;
-let millSpeedx = 0;
+//59,43,68 -- purple
+let canvasHeight = 768;
+let canvasWidth = 1024;
 
 function setup() {
-  createCanvas(1024, 768);
+  createCanvas(canvasWidth, canvasHeight);
 }
-//59,43,68 -- purple
 
-// function draw() {
-//   background(220);
-//   windmill(6, millSpeedx, "x", "rgb(59,43,68)", "rgb(97, 139, 74)");
-//   windmill(2, millSpeedy, "y", "rgb(154, 140, 152)", "rgb(124, 165, 184)");
-//   // adjust speed of windmills bases on mouse moving accross x and y axis
-//   millSpeedx += 5 + map(mouseX, 0, windowWidth, -10, 10);
-//   millSpeedy += 5 + map(mouseY, 0, windowHeight, -10, 10);
-// }
-
-// function windmill(loc, sp, ax, baseColor, bladeColor) {
-//   // windmill 1
-//   push();
-//   // move the origin to the left of the canvas
-//   translate(windowWidth / loc, windowHeight / loc);
-//   // windmill base
-//   fill(baseColor);
-//   beginShape();
-//   vertex(-15, -15);
-//   vertex(-50, 200);
-//   vertex(50, 200);
-//   vertex(15, -15);
-//   endShape(CLOSE);
-//   fill(0); // black text
-//   text("Speed controlled by " + ax + "-axis", -75, 220);
-
-//   // windmill blades
-//   fill(bladeColor);
-//   rotate(radians(sp));
-//   triangle(0, 0, 100, 50, 50, 100);
-//   triangle(0, 0, -100, -50, -50, -100);
-//   triangle(0, 0, -100, 50, -50, 100);
-//   triangle(0, 0, 100, -50, 50, -100);
-
-//   ellipse(0, 0, 30);
-//   pop();
-// }
+function draw(){
+  background(59,43,68);
+  fill(250,70,100);
+  stroke(59,43,68);
+  strokeWeight(3);
+  rect(0,(canvasHeight/10),(canvasWidth/2),(canvasHeight/10*4));
+  fill(36,80,214);
+  rect(((canvasWidth/4)-15),0,(canvasWidth/12),(canvasHeight/4)*3);
+  fill(53, 153, 146);
+  rect(((canvasWidth/4)+70),((canvasHeight/4)*1.3),canvasWidth-((canvasWidth/4)+70),100);
+  fill(111, 219, 210);
+  rect(canvasWidth/4+70,((canvasHeight/4)*1.3)+100,((canvasWidth/4)-71),canvasHeight-((canvasHeight/4)*1.3)+100);
+  fill(0,0,0,0);
+  stroke(100,49,130);
+  rect(canvasWidth/10,canvasHeight/10 + 20,(canvasWidth/10)*8,canvasHeight/10*7.6)
+}
