d7b48dbc87ac59c5cbc242669b2e0d6012ee58c3
Angela Carey <angela.k.carey@gmail.com>
2022-03-10 08:14:12 -0500

    clock changes

--- assignments/assignment_02/part_a/sketch.js
+++ assignments/assignment_02/part_a/sketch.js
@@ -1,6 +1,6 @@
-let hr = 0;
-let mn = 0;
-let sec = 0;
+let h = 0;
+let m = 0;
+let s = 0;
 const canvasSize = 800;
 
 // the rotation angle for the hands
@@ -13,69 +13,63 @@ function setup() {
 }
 
 function draw() {
-  background(59, 43, 68);  // purple
-  hr = hour();
-  mn = minute();
-  sec = second();
-  textSize(40);
-  textAlign(CENTER)
-  textFont('Helvetica');
-  strokeWeight(2);
-  stroke(255);
-  text('1',canvasSize/2,100);
-  text('3',canvasSize/12*11,canvasSize/2);
-  text('6',canvasSize/2,canvasSize/12 * 11);
-  text('9',canvasSize/12,canvasSize/2);
+  background(59, 43, 68); // purple
+  h = hour();
+  m = minute();
+  s = second();
+  // textSize(40);
+  // textAlign(CENTER)
+  // textFont('Helvetica');
+  // strokeWeight(2);
+  // stroke(255);
+  // text('1',canvasSize/2,100);
+  // text('3',canvasSize/12*11,canvasSize/2);
+  // text('6',canvasSize/2,canvasSize/12 * 11);
+  // text('9',canvasSize/12,canvasSize/2);
   drawDigital();
   translate(canvasSize / 2, canvasSize / 2);
   rotate(-90);
-
   drawClock();
-
 }
-function drawDigital(){
-
-
-    text(hr + ":" + nf(mn,2,0) + ":" + nf(sec,2,0), canvasSize/2, 50);
-
+function drawDigital() {
+  text(h + ":" + nf(m, 2, 0) + ":" + nf(s, 2, 0), canvasSize / 2, 50);
 }
 
 function drawClock() {
   noFill();
-
-
-  // Seconds display
   strokeWeight(8);
   stroke(97, 139, 74);
-  let secAngle = map(sec, 0, 60, 0, 360);
+  let sAngle = map(s, 0, 60, 0, 360);
+
+
   push();
   strokeWeight(11);
-  rotate(secAngle);
+  rotate(sAngle);
   // line(0, 0, canvasSize / 3, 0);
-  rect(0, 0, canvasSize/3, 10);
+  rect(0, 0, canvasSize / 3, 10);
   pop();
 
   // Minute display
   strokeWeight(8);
   stroke(124, 165, 184);
-  let minAngle = map(mn, 0, 60, 0, 360);
+  let mAngle = map(m, 0, 60, 0, 360);
   push();
   strokeWeight(20);
-  rotate(minAngle);
+  rotate(mAngle);
   // line(0, 0, canvasSize / 4, 0);
-  rect(0, 0, canvasSize/4, 10);
+  rect(0, 0, canvasSize / 4, 10);
 
   pop();
 
   // Hours display
   strokeWeight(8);
   stroke(154, 140, 152);
-  let hourAngle = map(hr % 12, 0, 12, 0, 360);
+  let hAngle = map(h % 12, 0, 12, 0, 360);
   push();
   strokeWeight(20);
-  rotate(hourAngle);
+  rotate(hAngle);
   // line(0, 0, canvasSize / 5, 0);
-  rect(0, 0, canvasSize/7, 10);
+  rect(0, 0, canvasSize / 7, 10);
 
   pop();
 
