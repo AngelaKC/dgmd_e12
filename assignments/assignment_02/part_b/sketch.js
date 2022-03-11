d1cb14094386a35bf73176bc0dcb2dcfad0cacc1
Angela Carey <angela.k.carey@gmail.com>
2022-03-11 09:34:07 -0500

    assg 2 part b

---
+++ assignments/assignment_02/part_b/sketch.js
@@ -0,0 +1,14 @@
+let h = 0;
+let m = 0;
+let s = 0;
+let canvasSize = 800;
+
+function setup() {
+  var canvas = createCanvas(canvasSize, canvasSize);
+  canvas.parent("sketch-holder");
+
+}
+
+function draw() {
+  background(59, 43, 68); // purple
+}
