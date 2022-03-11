a25efc5934171ef417e4b3ca591bcef69ef0da07
Angela Carey <angela.k.carey@gmail.com>
2022-03-11 11:35:23 -0500

    assgn 2 css sheet change

--- assignments/assignment_02/part_b/sketch.js
+++ assignments/assignment_02/part_b/sketch.js
@@ -7,7 +7,6 @@ let myColors = [
   "rgba(250,70,100, 0.5)", // pink - 0
   "rgba(36,80,214, 0.5)", // blue - 1
   "rgba(53, 153, 146, 0.5)", // teal - 2
-
 ];
 
 function setup() {
@@ -17,41 +16,43 @@ function setup() {
 
 function draw() {
   background(59, 43, 68); // purple
-  // background(255);
 
+  // set variables to hold time related values
   h = hour() * 10;
-  m = minute() *10;
-  s = second() *10;
-  // h = 10;
-  // m = 11;
-  // s = 11;
+  m = minute() * 10;
+  s = second() * 10;
   noStroke();
 
   /* Hour */
+  // Using push & pop to modify origins and rotations
   push();
   translate(-50, -50);
   fill(myColors[0]);
+  // rotates each hour
   rotate(h);
-  quad(h, 31, 86, h, -h, 63, h,-h);
+  // shape changes each hour
+  quad(h, 31, 86, h, -h, 63, h, -h);
   pop();
 
   /* Minute */
-
+  // Using push & pop to modify origins and rotations
   push();
   translate(-50, -150);
   fill(myColors[1]);
-
+  // rotates each minute
   rotate(m);
-  quad(m, 31, m, m, -m, 63, -m,-m);
+  // shape changes each hour
+  quad(m, 31, m, m, -m, 63, -m, -m);
   pop();
 
   /* Second */
+  // Using push & pop to modify origins and rotations
   push();
   translate(-100, -100);
   fill(myColors[2]);
-
+  // rotates each minute
   rotate(s);
-  quad(s, 31, 86, s, -s, 63, s,-s);
-
+  // shape changes each hour
+  quad(s, 31, 86, s, -s, 63, s, -s);
   pop();
 }
