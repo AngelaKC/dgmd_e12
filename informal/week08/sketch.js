87ca91d47540cc264e94ff02bbe5652d6f6901f0
Angela Carey <angela.k.carey@gmail.com>
2022-03-29 09:07:15 -0400

    Informal Week 8/9

--- informal/week08/sketch.js
+++ informal/week08/sketch.js
@@ -1,19 +1,47 @@
-let pom;
-let slider;
+let pom, choice;
+let slider, radio;
+let fruit = [];
+let img;
 
 function setup() {
-  createCanvas(600, 600);
+  canvas=createCanvas(600, 625);
+  canvas.parent("sketch-holder")
   slider = createSlider(20, 75, 50);
-  slider.position(220, 10);
+  slider.position(250, 15);
   slider.style('width', '80px');  
+  radio = createRadio();
+  radio.option(0, 'apple');
+  radio.option(1, 'pineapple');
+  radio.option(2, 'strawberry');
+  radio.style('width', '300px');
+  radio.selected('0');
+  radio.parent("radio");
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
   text("Change Pom Size: " , 20, 25);
-  // text("X: " + mouseX, 20, 30);
-  // text("Y: " + mouseY, 20, 50);
   pom = slider.value();
+  choice = radio.value();
+  drawMe();
+  img = fruit[choice];
+
+  for(let i = 0; i<10;i++){
+    image(img,(100 + i*45),500);
+  }
+
+
+}
+
+//preload runs before the sketch starts, and loads assets!
+function preload() {
+  fruit[0] = loadImage('images/apple.png');
+  fruit[1] = loadImage('images/pineapple.png');
+  fruit[2] = loadImage('images/strawberry.png');
+}
+
+function drawMe(){
   textStyle(normal);
   textSize(24);
   fill(250, 235, 205);
