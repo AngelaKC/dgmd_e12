3a5f9ec73837441e11d27ae714409d646d51f85a
Angela Carey <angela.k.carey@gmail.com>
2022-04-07 09:36:52 -0400

    update comments

--- assignments/assignment_03/sketch.js
+++ assignments/assignment_03/sketch.js
@@ -46,40 +46,59 @@ let circle = {
     ellipse(this.x, this.y, this.circleSize, this.circleSize * 3);
   },
 };
-function preload() {}
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
+  // positions the canvas and used z-index to
+  // make it a back ground image
+  // This is my canvas element
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
   noStroke();
   // get user input
+  // Creating an Input Element
   textField = select("#name");
   textField = createInput();
+  // using position() to control location
   textField.position(145, 30);
+  // using parent 
   textField.parent("name");
   // choose color palette
+  // creating Radio Button element
+  // allows user to choose color palette
   radio = createRadio();
   radio.option("1", "blue");
   radio.option("2", "pink");
   radio.option("3", "multi");
   radio.selected("1");
+  // using style() to control look & feel
   radio.style("width", "60px");
-  radio.position( 145,60);
+  // using position() to control location
+  radio.position(145, 60);
   textAlign(CENTER);
+  // using parent() 
   radio.parent("color");
-
+  // creating start/stop controller 
+  // creating Button element
   output = select("#output");
   output.position(windowWidth / 4, windowHeight / 2);
   button = createButton("Start/Stop");
+  // using position() to control location
   button.position(160, 135);
+   // using parent() 
   button.parent("input");
-  button.mousePressed(newName);
+  // using element-specific event handler 
+  // and callback function
+  button.mousePressed(startTransformation);
 }
 
 function draw() {
   let newCircleSize = round(random(1, 200));
+  // allows user to toggle transformation on/off
+  // only runs when toggle is true
   if (clickToggle) {
     noStroke();
+    // sets color palette according to 
+    // radio button selection
     let palette = radio.value();
     if (palette == 1) {
       fill(myBlues[round(random(0, 4))]);
@@ -88,8 +107,9 @@ function draw() {
     } else {
       fill(myColors[round(random(0, 4))]);
     }
-
+  // displays circles
     circle.display();
+    // changes circle sizes ro random sizes
     randomSize = circle.change(
       round(random(0, windowWidth)),
       round(random(0, windowHeight)),
@@ -97,11 +117,14 @@ function draw() {
     );
   }
 }
-function newName() {
+function startTransformation() {
+  // sets message to include user input
   let message = "Happy Birthday " + textField.value() + "!!!";
+  // uses html() to modify content of 
+  // DOM element
   output.html(message);
+  // calls toggler() function
   toggler();
-  
 }
 // this function sets the clickToggle variable when mouse is clicked
 function toggler() {
