aef96410ab2738424f43994662b84ca823325646
Angela Carey <angela.k.carey@gmail.com>
2022-04-16 09:15:22 -0400

    assignment 4 created

--- assignments/assignment_04/sketch.js
+++ assignments/assignment_04/sketch.js
@@ -1,64 +1,139 @@
-// set base api URL
-let api = "https://www.boredapi.com/api/activity";
-// Create variables for use in program
-let myActivity;
-let activityType, link;
-// Create array to hold drop down values
-let activityTypes = [
-  "Choose Activity Type",
-  "busywork",
-  "charity",
-  "cooking",
-  "diy",
-  "education",
-  "music",
-  "recreational",
-  "relaxation",
-  "social",
+let canvasWidth = 1000;
+let canvasHeight = 500;
+let myColors = [
+  "rgb(255, 190, 11)", // 0
+  "rgb(251, 86, 7)", // 1
+  "rgb(255, 0, 110)", // 2
+  "rgb(131, 56, 236)", // 3
+  "rgb(58, 134, 255)", // 4
 ];
+let myPinks = [
+  "rgb(255,0,129)", // 0
+  "rgb(255,72,165)", // 1
+  "rgb(255,119,188)", // 2
+  "rgb(255,174,215)", // 3
+  "rgb(255,202,229)", // 4
+];
+let myBlues = [
+  "rgb(0,255,255)", // 0
+  "rgb(0,219,255)", // 1
+  "rgb(16,165,245)", // 2
+  "rgb(12,113,224)", // 3
+  "rgb(8,89,198)", // 4
+];
+let textField, output, button;
+let clickToggle = false;
+let circle = {
+  // circle properties
+  x: canvasWidth / 2,
+  y: canvasHeight / 2,
+  circleSize: 5,
 
+  // METHODS of circle object
+  change: function (newX, newY, newSize) {
+    // pass in values to change location and size
+    this.x = newX;
+    this.y = newY;
+    this.circleSize = newSize;
+
+    // constrain() is a P5 function that limits the range of a var
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+  },
+
+  display: function () {
+    // draw the circle with at the current location and size
+    ellipse(this.x, this.y, this.circleSize, this.circleSize * 3);
+  },
+};
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // create drop down functionality for user
-  activitySel = createSelect();
-  activitySel.position(20, 120);
-  // use array of values to create Select options
-  for (let i = 0; i < activityTypes.length; i++) {
-    activitySel.option(activityTypes[i]);
-  }
-  // create changed event
-  activitySel.changed(typeSelEvent);
-  // configure text
-  fill(59, 43, 68);
-  strokeWeight(0);
-  textStyle(NORMAL);
-  textSize(35);
-  textFont("Arial");
-}
-// Action taken when user changes selection
-function typeSelEvent() {
-  activityType = activitySel.value();
-  // concatenate user selected type to base api url
-  typeUrl = api + "?type=" + activityType;
-  // test url to see link applied
-  // typeUrl = api + "?key=4522866" ;
-  // get data, call a callback function we called 'gotData' when data received
-  loadJSON(typeUrl, activityData);
+  var canvas = createCanvas(windowWidth, windowHeight);
+  // positions the canvas and used z-index to
+  // make it a back ground image
+  // This is my canvas element
+  canvas.position(0, 0);
+  canvas.style("z-index", "-1");
+  noStroke();
+  // get user input
+  // Creating an Input Element
+  textField = select("#name");
+  textField = createInput();
+  // using position() to control location
+  textField.position(145, 30);
+  // using parent 
+  textField.parent("name");
+  // choose color palette
+  // creating Radio Button element
+  // allows user to choose color palette
+  radio = createRadio();
+  radio.option("1", "blue");
+  radio.option("2", "pink");
+  radio.option("3", "multi");
+  radio.selected("1");
+  // using style() to control look & feel
+  radio.style("width", "60px");
+  // using position() to control location
+  radio.position(145, 60);
+  textAlign(CENTER);
+  // using parent() 
+  radio.parent("color");
+  // creating start/stop controller 
+  // creating Button element
+  output = select("#output");
+  output.position(windowWidth / 4, windowHeight / 2);
+  button = createButton("Start/Stop");
+  // using position() to control location
+  button.position(160, 135);
+   // using parent() 
+  button.parent("input");
+  // using element-specific event handler 
+  // and callback function
+  button.mousePressed(startTransformation);
 }
 
-// the callback from loadJSON sends 'data' automatically!
-function activityData(myData) {
-  myActivity = myData;
-  // if data loads print to screen
-  if (myActivity) {
-    // make the temperature value the radius rather than diameter
-    clear();
-    fill(59, 43, 68);
-    // create an anchor tag to hold value if link is populated
-    link = createA(myActivity.link, myActivity.link);
-    text(myActivity.activity, 16, 100);
-    link.position(22, 250);
+function draw() {
+  let newCircleSize = round(random(1, 200));
+  // allows user to toggle transformation on/off
+  // only runs when toggle is true
+  if (clickToggle) {
+    noStroke();
+    // sets color palette according to 
+    // radio button selection
+    let palette = radio.value();
+    if (palette == 1) {
+      fill(myBlues[round(random(0, 4))]);
+    } else if (palette == 2) {
+      fill(myPinks[round(random(0, 4))]);
+    } else {
+      fill(myColors[round(random(0, 4))]);
+    }
+  // displays circles
+    circle.display();
+    // changes circle sizes ro random sizes
+    randomSize = circle.change(
+      round(random(0, windowWidth)),
+      round(random(0, windowHeight)),
+      newCircleSize
+    );
   }
 }
-// not needed for this application
-function draw() {}
+function startTransformation() {
+  // sets message to include user input
+  let message = "Happy Birthday " + textField.value() + "!!!";
+  // uses html() to modify content of 
+  // DOM element
+  output.html(message);
+  // calls toggler() function
+  toggler();
+}
+// this function sets the clickToggle variable when mouse is clicked
+function toggler() {
+  if (clickToggle) {
+    // if value is true change to false
+    clickToggle = false;
+  } else {
+    clear();
+    // else value is false and change to true
+    clickToggle = true;
+  }
+}
