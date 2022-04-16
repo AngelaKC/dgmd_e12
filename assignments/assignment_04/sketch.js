5b143abffdb3cee0b4e370cc0a977c60902211dd
Angela Carey <angela.k.carey@gmail.com>
2022-04-16 11:03:08 -0400

    assignment 4 created

--- assignments/assignment_04/sketch.js
+++ assignments/assignment_04/sketch.js
@@ -1,54 +1,32 @@
 let canvasWidth = 1024;
-let canvasHeight = 684;
+let canvasHeight = 768;
 let myBackgrounds = [];
 let myImgUrls = [
-  "images/blackWithBdayCandles.jpg", // 0
-  "images/blueAstronautCupcakes.jpg", // 1
-  "images/blueBricks.jpg", // 2
-  "images/blueConfetti.jpg", // 3
+  "images/astronaut.jpg", // 0
+  "images/blueBricks.jpg", // 1
+  "images/blueConfetti.jpg", // 2  
+  "images/candles.jpg", // 3
   "images/dinosaurParty.jpg", // 4
-  "images/handWithMultiColorBalloons.jpg", // 5
-  "images/neutralCakeWithPopcorn.jpg", // 6
-  "images/pinkBalloons.jpg", // 7
-  "images/pinkBluePurpleSprinkles.jpg", // 8
+  "images/multiColorBalloons.jpg", // 5
+  "images/neutralCake.jpg", // 6
+  "images/paperPackages.jpg", // 7
+  "images/pinkBalloons.jpg", // 8
   "images/pinkIcing.jpg", // 9
-  "images/prettyPinkPackages.jpg", // 10
-  "images/rainbowSprinkles.jpg", // 11
-  "images/sharks.jpg", // 12
-  "images/whiteCupcakeonwhiteboards.jpg", // 13
-  "images/whiteWithBrownPackages.jpg", // 14
-]
+  "images/rainbowSprinkles.jpg", // 10
+  "images/sharks.jpg", // 11
+  "images/sprinkles.jpg", // 12
+  "images/whiteCupcakes.jpg", // 13
+];
 let textField, output, button;
 let clickToggle = false;
-let circle = {
-  // circle properties
-  x: canvasWidth / 2,
-  y: canvasHeight / 2,
-  circleSize: 5,
-
-  // METHODS of circle object
-  change: function (newX, newY, newSize) {
-    // pass in values to change location and size
-    this.x = newX;
-    this.y = newY;
-    this.circleSize = newSize;
-
-    // constrain() is a P5 function that limits the range of a var
-    this.x = constrain(this.x, 0, width);
-    this.y = constrain(this.y, 0, height);
-  },
-
-  display: function () {
-    // draw the circle with at the current location and size
-    ellipse(this.x, this.y, this.circleSize, this.circleSize * 3);
-  },
-};
-function preload(){
-  for(let i=0; i < myImgUrls.length;i++){
-    myBackgrounds[i]=loadImage(myImgUrls[i]);
+// preload images
+function preload() {
+  for (let i = 0; i < myImgUrls.length; i++) {
+    myBackgrounds[i] = loadImage(myImgUrls[i]);
   }
 }
 function setup() {
+  // var canvas = createCanvas(canvasWidth, canvasHeight);
   var canvas = createCanvas(windowWidth, windowHeight);
   // positions the canvas and used z-index to
   // make it a back ground image
@@ -61,49 +39,49 @@ function setup() {
   textField.position(145, 30);
   textField.parent("name");
 
+  // creating drop down options
+  sel = createSelect();
+  sel.position(10, 10);
+  sel.option("Select Theme", "100");
+  sel.option("Astronaut Cupcakes", "0");
+  sel.option("Blue Bricks", "1");
+  sel.option("Blue Confetti", "2");
+  sel.option("Birthday Candles", "3");
+  sel.option("Dinosaur Party", "4");
+  sel.option("Multi Color Balloons", "5");
+  sel.option("Cake", "6");
+  sel.option("Paper Packages", "7");
+  sel.option("Pink Balloons", "8");
+  sel.option("Pink Icing", "9");
+  sel.option("Rainbow Sprinkles", "10");
+  sel.option("Sharks", "11");
+  sel.option("Sprinkles", "12");
+  sel.option("White Cupcakes", "13");
+  // sel.selected('candles');
+  sel.changed(mySelectEvent);
   // creating Button element
   output = select("#output");
   output.position(windowWidth / 4, windowHeight / 2);
   button = createButton("Start/Stop");
   // using position() to control location
   button.position(160, 135);
-   // using parent() 
+  // using parent()
   button.parent("input");
-  // using element-specific event handler 
+  // using element-specific event handler
   // and callback function
   button.mousePressed(startTransformation);
 }
 
-function draw() {
-  let newCircleSize = round(random(1, 200));
-  // allows user to toggle transformation on/off
-  // only runs when toggle is true
-  if (clickToggle) {
-    noStroke();
-    // sets color palette according to 
-    // radio button selection
-    let palette = radio.value();
-    if (palette == 1) {
-      fill(myBlues[round(random(0, 4))]);
-    } else if (palette == 2) {
-      fill(myPinks[round(random(0, 4))]);
-    } else {
-      fill(myColors[round(random(0, 4))]);
-    }
-  // displays circles
-    circle.display();
-    // changes circle sizes ro random sizes
-    randomSize = circle.change(
-      round(random(0, windowWidth)),
-      round(random(0, windowHeight)),
-      newCircleSize
-    );
-  }
+function draw() {}
+
+function mySelectEvent() {
+  // imageMode(CENTER);
+  image(myBackgrounds[sel.value()], 0, 0);
 }
 function startTransformation() {
   // sets message to include user input
   let message = "Happy Birthday " + textField.value() + "!!!";
-  // uses html() to modify content of 
+  // uses html() to modify content of
   // DOM element
   output.html(message);
   // calls toggler() function
