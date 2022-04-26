b55436e1bd36e777c97bf1e3681ae8f96ab7ea81
Angela Carey <angela.k.carey@gmail.com>
2022-04-26 11:12:58 -0400

    assignment 4 JSON File

--- assignments/assignment_04/sketch.js
+++ assignments/assignment_04/sketch.js
@@ -1,71 +1,79 @@
 let canvasWidth = 1024;
 let canvasHeight = 768;
-// let myBackgrounds = [];
-// let myConfetti = [];
-let confetti;
 let textField, output, button;
-let sel, themeChoice;
+let themeSel, themeChoice;
 let clickToggle = false;
-let myBG;
-let myFile = [];
+let imgPath, myBG;
+let textX, textY, textFont, textSize;
+let fontWeight, fontColor;
 
-function preload() {
-  // myFile = loadJSON("data/themes.json");
-}
 function setup() {
   var canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.parent("sketch-holder");
-  // positions the canvas and used z-index to
+  // positions the canvas and uses z-index to
   // make it a back ground image
+  canvas.parent("sketch-holder");
   canvas.style("z-index", "-1");
-  noStroke();
   // get user input
   textField = select("#name");
-  textField = createInput();
-  textField.position(145, 30);
+  textField = createInput("Enter Name");
+  textField.position(20, 30);
   textField.parent("name");
 
   // creating drop down options
-  sel = createSelect();
-  sel.position(10, 10);
-  sel.option("Select Theme", "100");
-  sel.option("Blue Bricks", "0");
-  sel.option("Birthday Candles", "1");
-  sel.option("Pink Icing", "2");
-  sel.changed(mySelectEvent);
+  themeSel = createSelect();
+  themeSel.position(180, 30);
+  themeSel.option("Select Theme", "100");
+  themeSel.option("Blue Bricks", "0");
+  themeSel.option("Birthday Candles", "1");
+  themeSel.option("Pink Icing", "2");
+  // Use callback function whenever user changes 
+  // selection 
+  themeSel.changed(mySelectEvent);
 
-
-  // // creating Button element
-  // output = select("#output");
-  // output.position(windowWidth / 4, windowHeight / 2);
-  // button = createButton("Start/Stop");
-  // // using position() to control location
-  // button.position(160, 135);
-  // // using parent()
-  // button.parent("input");
-  // // using element-specific event handler
-  // // and callback function
-  // button.mousePressed(startTransformation);
+  // creating Button element
+  button = createButton("Start/Stop");
+  button.position(320, 30);
+  button.parent("input");
+  // using element-specific event handler
+  // and callback function
+  button.mousePressed(startTransformation);
 }
 
-function draw() {}
+function draw() {
+  if (myBG) {
+    image(myBG, 0, 0);
+    output = select("#output");
+    // adjust style and position based
+    // on theme choices from JSON
+    output.position(windowWidth/textX, windowHeight/textY);
+    output.style('font-size',textSize )
+    output.style('font-family', textFont)
+    output.style('font-weight', fontWeight)
+    output.style('color',fontColor )
+  }
+
+}
 function mySelectEvent() {
-   themeChoice = sel.value();
-  console.log(themeChoice);
-  // imageMode(CENTER);
-
-  // loadJSON("data/themes.json",processTheme(sel.val));
-
-  // image(myFile.id[sel.value].photo, 0, 0);
+  themeChoice = themeSel.value();
+  // loads JSON file and uses callback function to
+  // do something with the data
+  loadJSON("data/themes.json", processTheme);
 }
-function processTheme(themeNum) {
-  console.log(themeNum);
+function processTheme(data) {
+
+  imgPath = data.themes[themeChoice].photo;
+  myBG = loadImage(imgPath);
+  textX = data.themes[themeChoice].textXLoc;
+  textY = data.themes[themeChoice].textYLoc;
+  textFont = data.themes[themeChoice].font;
+  textSize = data.themes[themeChoice].fontSize;
+  fontWeight = data.themes[themeChoice].fontWeight;
+  fontColor = data.themes[themeChoice].textColor;
 }
 function startTransformation() {
   // sets message to include user input
-  let message = "Happy Birthday " + textField.value() + "!!!";
-  // uses html() to modify content of
-  // DOM element
+  let message = "Happy Birthday <br>" + textField.value() + "!!!";
+  // uses html() to modify content of DOM element
   output.html(message);
   // calls toggler() function
   toggler();
